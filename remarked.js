a4ae283c70f4182235bc8a5b30b151b947fcc4e2
Michael <mikejonesab1@yahoo.com>
2017-05-03 10:16:20 -0700

    Added new HTML to MD converters.

--- remarked.js
+++ remarked.js
@@ -1,80 +1,50 @@
 var JSDOM = require('jsdom').JSDOM,
-    Converters = require('./lib/converters.js');
+	Converters = require('./lib/converters.js');
 
-function Remarked(){
+function Remarked() {
 
 }
 
-Remarked.prototype.convert = function (html){
-    var result;
+Remarked.prototype.convert = function(html) {
+	var result;
 
-    if(typeof html !== 'string') throw 'HTML must be a string.';
+	if (typeof html !== 'string') throw 'HTML must be a string.';
 
-    this.DOM = new JSDOM(html);
-    this.body = this.DOM.window.document.body;
+	this.DOM = new JSDOM(html);
+	this.body = this.DOM.window.document.body;
 
-    //this.nodes = this.flattenDom(this.body, []);
-    result = this.build(this.body,{});
+	result = this.build(this.body, {md:'', block:''});
 
-    return result;
+	return result.block;
 };
 
-Remarked.prototype.build = function (node, result){
-	var children = Array.prototype.slice.call(node.childNodes),
+Remarked.prototype.build = function(node, result) {
+	var self = this,
+		children = Array.prototype.slice.call(node.childNodes),
 		childMd;
 
-	if(node.nodeType === 3){
+	if (node.nodeType === 3) {
 		result.md = node.textContent.replace(/[\r\n\t].*?/g, '');
 	}
 
-	childMd = children.reduce(function(text, child){
-	       var newResult = this.build(child, result);
-           result.md += newResult;
-	},'');
+	childMd = children.reduce(function(text, child) {
+		return text += self.build(child, result).md;
+	}, '');
 
-	if (node.nodeType === 1){
+	if (node.nodeType === 1 && !self.blockElements.includes(node.tagName)) {
 		result.md = (self.converters[node.tagName] || self.converters['default'])(childMd, node);
 	}
 
-    return result;
+	if (node.nodeType === 1 && self.blockElements.includes(node.tagName)) {
+		result.block += (self.converters[node.tagName] || self.converters['default'])(childMd, node);
+		result.md = '';
+	}
 
-    /*var self = this,
-        nodesArray = Array.prototype.slice.call(nodes);
-
-    return nodesArray.reduce(function (result, node){
-        var childArray = Array.prototype.slice.call(node.childNodes),
-            text;
-
-        if (node.nodeType === 3) {
-            node._md = node.textContent.replace(/[\r\n\t].*?/g, '');
-        }
-
-        text = childArray.reduce(function(text, child){
-            return text += child._md;
-        },'');
-
-        if (node.nodeType === 1){
-            node._md = (self.converters[node.tagName] || self.converters['default'])(text, node);
-        }
-
-        return node._md;
-    },'');*/
-};
-
-Remarked.prototype.flattenDom = function (node, nodes){
-    var self = this;
-    nodes.unshift(node);
-
-    node.childNodes.forEach(function (child){
-        self.flattenDom(child, nodes);
-    });
-
-    return nodes;
+	return result;
 };
 
 Remarked.prototype.blockElements = [
-    'BODY',
-    'DIV'
+	'DIV'
 ];
 
 Converters(Remarked);
